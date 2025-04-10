57b37948c317a2da7d3d1593e405b3479ac1fd6f
OannaKenza <oannakenzaihirwe@gmail.com>
2025-04-10 16:57:02 +0200

    added a function to add a categorized task

--- day3.js
+++ day3.js
@@ -12,5 +12,19 @@ function displayCategorizedTasks(){
     })
 }
 
+function addCategorizedTasks(category,task){
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i][0] === category){
+            tasks[i][1].push(task);
+            console.log(`\n Task "${task}" added to "${category}" tasks \n`);
+            console.log(" To do list after adding a new task");
+            return;
+        }
+    }
+    console.log(`Category "${category}" not found \n`)
+}
+console.log("To do list before adding a new task \n")
+displayCategorizedTasks()
 
+addCategorizedTasks("Work" , "Complete the presentation")
 displayCategorizedTasks()
