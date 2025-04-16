5585f7194a693a4a51042182630fbe277a4a87c6
OannaKenza <oannakenzaihirwe@gmail.com>
2025-04-16 16:38:25 +0200

    feat:understanding properties and methods

--- week12/version1.js
+++ week12/version1.js
@@ -26,11 +26,37 @@ class Task {
         console.log(`${this.completed ? "\tTask is completed" : "\tTask is not completed"}`)
         console.log("+-----------------------------------------------------------------------------+");
     }
+
+    // updateTask(newTtitle,newDescription,newCategory){
+    //     this.title=newTtitle;
+    //     this.description=newDescription;
+    //     this.category=newCategory;
+    //     console.log("Task updated successfully");
+
+    //}
+
+    updateTaskTitle(newTitle){
+        this.title = newTitle;
+        console.log("\tTask title updated successfully");
+    }
+    updateTaskDescription(newDescription){
+        this.description = newDescription;
+        console.log("\tTask description updated successfully");
+    }
+    updateTaskCategory(newCategory){
+        this.category = newCategory;
+        console.log("\tTask category updated successfully");
+    }
+
 }
+
 
 let task1 = new Task("Buy grocceries", "Get milk,bread and eggs from the store.","Shopping task");
 
 task1.displayDetails();
+
+task1.updateTaskTitle("Buy grocceries - 1");
+console.log("\tUpdated task details");
 
 task1.toggleCompleted();
 
