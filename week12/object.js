bbe66aca234fed81f86ccd94d376ad474878beff
OannaKenza <oannakenzaihirwe@gmail.com>
2025-04-20 12:29:50 +0200

    feat:understanding factory functions and their properties

--- week12/object.js
+++ week12/object.js
@@ -18,6 +18,21 @@ function createTask(title, description ,category){
             console.log(`\t${this.completed ? "Completed" : "Not Completed"}`);
             console.log("+-----------------------------------------------------------------+");
         },
+
+        updateTitle(newTitle){
+            this.title = newTitle;
+            console.log(`Task title updated successfully`);
+        },
+        
+        updateDescription(newDescription){
+            this.description = newDescription;
+            console.log(`Task description updated successfully`);
+        },
+
+        updateCategory(newCategory){
+            this.category = newCategory;
+            console.log(`Task category updated successfully`);
+        }
     }
 }
 
@@ -25,5 +40,17 @@ let task1 = createTask("Buy groceries", "Get milk,eggs and bread", "Shopping");
 task1.displayDetails();
 
 task1.toggleCompleted();
-console.log("New task details");
+console.log("| \t New task details \t |");
+task1.displayDetails();
+
+task1.updateTitle("Buy groceries -1");
+console.log("\t Updated task details \t");
+task1.displayDetails();
+
+task1.updateDescription("Get milk,eggs and bread -1");
+console.log("\t Updated task details \t");
+task1.displayDetails();
+
+task1.updateCategory("Shopping -1");
+console.log("\t Updated task details \t");
 task1.displayDetails();
